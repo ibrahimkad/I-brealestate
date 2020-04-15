d55ad70e5640197a00470a8b64d598f7cde155b7
ibrahimkad <ibrahim.kaddumi@gmail.com>
2020-04-15 18:23:56 +0300

    lab08

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+'use strict'
+
+var order = prompt('you want house or mansion');
+while (order !== 'house' && order !== 'mansion') {
+    order = prompt('please write house or mansion');
+}
+
+var numbers = prompt('please enter the numbers of buildings that you want to show');
+var item = '';
+var showHouse = function () {
+    for (var i = 0; i < numbers; i++) {
+        console.log(numbers);
+        
+        if (order === "house") {
+            console.log(order)
+            item = item + '<img src="pic/house.jpg">';
+        } else if (order === "mansion") {
+            item = item + '<img src ="pic/mansion.jpg">';
+        }// else {
+           // item = "order not found";
+       // }
+          
+          
+          console.log(numbers)
+    }
+    return item;
+}  // document.write(showHouse);
